01cc29defab38e000374962a549ff7e6b3c8cf5a
clandau <so.super@gmail.com>
2018-04-08 23:28:33 -0400

    initial commit

---
+++ ageInBinary.js
@@ -0,0 +1,16 @@
+function ageInBinary(age) {
+    if(isNaN(age)) {
+        return('not a number');
+    }
+    else if(age > 140 || age < 0) return('invalid age');
+    else if(!Number.isInteger(age)) return('please enter a whole number');
+    let binaryAge = [];
+    while(age > 0) {
+        var remain = age % 2;
+        age = parseInt(age/2);
+        binaryAge.unshift(remain.toString());
+    }
+    return binaryAge.join('');
+}
+
+console.log(ageInBinary(30.343));
